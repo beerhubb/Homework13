ba73002cd962afac573fac86ba47be31b85593ae
Chaiyawat Maduea <51731963+beerhubb@users.noreply.github.com>
2019-11-11 13:43:03 +0700

    Add files via upload

---
+++ IHomework13.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace no_1
+{
+    public interface IHomework13
+    {
+        IEnumerable<int> Homework13(IEnumerable<int> numbers);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace no_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new IHomework();
+            var num = new List<int> { 992889, 455998, 212299, 998875, 000993};
+            var result = new List<string>();
+            var sut = test.Homework13(num);
+            foreach (var item in sut)
+            {
+                var ss = item.ToString();
+                if (ss.Length == 5)
+                {
+                    ss = "0" + ss;
+                }
+                else if (ss.Length == 4)
+                {
+                    ss = "00" + ss;
+                }
+                else if (ss.Length == 3)
+                {
+                    ss = "000" + ss;
+                }
+                else if (ss.Length == 2)
+                {
+                    ss = "0000" + ss;
+                }
+                else if (ss.Length == 1)
+                {
+                    ss = "00000" + ss;
+                }
+                result.Add(ss);
+            }
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
